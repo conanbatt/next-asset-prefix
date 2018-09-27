2507683f600161ed92130a83494f2eccea17eb68
gabriel <gabriel@circlemedical.com>
2018-09-27 12:19:20 -0700

    dirty

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const next = require('next');
+
+const app = next({ dev: true });
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  // Entire site will be served under subdomain.
+  // Pages are using file api, all pages under pages/subdomain/etc
+  app.setAssetPrefix('/subpath');
+
+  server.use('/subpath', (req, res) => {
+    const { path } = req;
+    console.log('hit sub path');
+    handle(req, res);
+  });
+
+  server.get('*', (req, res) => {
+    const { path } = req;
+    handle(req, res);
+  });
+
+  server.listen(4444, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on ${4444}`);
+  });
+});
